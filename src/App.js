053c91934d72792a53d59c7f5c40f67594e7dc4f
raheemelassadi <raheemelassadi1@gmail.com>
2023-05-15 02:19:19 -0700

    changed background color and added hero text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import './app.scss'
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import './hero.scss'
+
+const Hero = () => {
+    return (
+        <>
+            <div id='hero'>
+                <h1>RAHEEM <br /><span id='last-name'>EL-ASSADI</span></h1>
+            </div>
+        </>
+    )
+}
+
+
+export default Hero
