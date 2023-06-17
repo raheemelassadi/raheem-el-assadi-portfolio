c758389a37d3711e7b3216a44f4207738137d0b4
raheemelassadi <raheemelassadi1@gmail.com>
2023-06-16 19:24:55 -0700

    fixed styling for hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,18 +2,22 @@ import './hero.scss'
 
 const Hero = () => {
   return (
+    <>
     <div className='main'>
       <div className="container">
-        <span>Software</span>
+        <p>Software</p>
         <div className="stack" style={{ '--stacks': 3 }}>
           <span style={{ '--index': 0 }}>RAHEEM EL-ASSADI</span>
           <span style={{ '--index': 1 }}>RAHEEM EL-ASSADI</span>
           <span style={{ '--index': 2 }}>RAHEEM EL-ASSADI</span>
         </div>
-        <span className="right">Developer</span>
-        <span>&#xf0c9;</span>
-      </div>
+        <div className='right'>
+        <p>Developer</p>
+        </div>
+      </div>      
     </div>
+    <p className='arrow'><i class="gg-push-down"></i></p>
+    </>
   )
 }
 
