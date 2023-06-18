7343ca665f4f320d5e148b3760237392b8fc9065
raheemelassadi <raheemelassadi1@gmail.com>
2023-06-18 03:59:31 -0700

    added main body for content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar";
-import './app.scss'
 import Hero from "./components/Hero";
+import Main from "./components/Main";
+import './app.scss'
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import './main.scss'
+
+const Main = () => {
+    return(
+        <div className='main'>
+            <h1>this is the main body</h1>
+        </div>
+    )
+}
+
+export default Main
