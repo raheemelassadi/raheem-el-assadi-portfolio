b823d2fcd76d8a6d1557f40571cb96d7a4e9c929
raheemelassadi <raheemelassadi1@gmail.com>
2023-06-25 05:43:21 -0700

    fixed skills section layout using grid instead of flexbox

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,18 +6,46 @@ const Skills = () => {
             <div className="skills-container">
                 <h2>Skills</h2>
                 <div className="skills-items">
-                    <ul>
-                        <li className='skills-li'><i class="devicon-html5-plain skills-icon"></i><span className='skills-text'>html</span></li>
-                        <li className='skills-li'><i class="devicon-css3-plain skills-icon"></i><span className='skills-text'>css</span></li>
-                        <li className='skills-li'><i class="devicon-javascript-plain skills-icon"></i><span className='skills-text'>javascript</span></li>
-                        <li className='skills-li'><i class="devicon-sass-original skills-icon"></i><span className='skills-text'>sass</span></li>
-                        <li className='skills-li'><i class="devicon-react-original skills-icon"></i><span className='skills-text'>react</span></li>
-                        <li className='skills-li'><i class="devicon-git-plain skills-icon"></i><span className='skills-text'>git</span></li>
-                        <li className='skills-li'><i class="devicon-github-original skills-icon"></i><span className='skills-text'>github</span></li>
-                        <li className='skills-li'><i class="devicon-nodejs-plain skills-icon"></i><span className='skills-text'>nodejs</span></li>
-                        <li className='skills-li'><i class="devicon-express-original skills-icon"></i><span className='skills-text'>express</span></li>
-                        <li className='skills-li'><i class="devicon-jest-plain skills-icon"></i><span className='skills-text'>jest</span></li>
-                    </ul>
+                        <div className='skills-li'>
+                            <i class="devicon-html5-plain skills-icon"></i>
+                            <span className='skills-text'>html</span>
+                        </div>
+                        <div className='skills-li'>
+                            <i class="devicon-css3-plain skills-icon"></i>
+                            <span className='skills-text'>css</span>
+                        </div>
+                        <div className='skills-li'>
+                            <i class="devicon-javascript-plain skills-icon"></i>
+                            <span className='skills-text'>javascript</span>
+                        </div>
+                        <div className='skills-li'>
+                            <i class="devicon-sass-original skills-icon"></i>
+                            <span className='skills-text'>sass</span>
+                        </div>
+                        <div className='skills-li'>
+                            <i class="devicon-react-original skills-icon"></i>
+                            <span className='skills-text'>react</span>
+                        </div>
+                        <div className='skills-li'>
+                            <i class="devicon-git-plain skills-icon"></i>
+                            <span className='skills-text'>git</span>
+                        </div>
+                        <div className='skills-li'>
+                            <i class="devicon-github-original skills-icon"></i>
+                            <span className='skills-text'>github</span>
+                        </div>
+                        <div className='skills-li'>
+                            <i class="devicon-nodejs-plain skills-icon"></i>
+                            <span className='skills-text'>nodejs</span>
+                        </div>
+                        <div className='skills-li'>
+                            <i class="devicon-express-original skills-icon"></i>
+                            <span className='skills-text'>express</span>
+                        </div>
+                        <div className='skills-li'>
+                            <i class="devicon-jest-plain skills-icon"></i>
+                            <span className='skills-text'>jest</span>
+                        </div>
                 </div>
             </div>
         </>
